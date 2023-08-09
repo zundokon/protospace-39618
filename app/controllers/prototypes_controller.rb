21d67481f3292e10863b03c37abf096820d56c3a
zundokon <tete5337@yahoo.co.jp>
2023-08-09 11:15:25 +0900

    新規投稿画面の表示・ルーティング

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,6 +2,10 @@ class PrototypesController < ApplicationController
   def index
   end
 
+  def new
+    @prototype = Prototype.new
+  end
+
   def prototype_params
     params.require(:prototype).permit(:name, :image, :text).merge(user_id: current_user.id)
   end
