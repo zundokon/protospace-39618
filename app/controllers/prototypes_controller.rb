e233469bf12b5c4c4da7c4d48ef85d754633959e
zundokon <tete5337@yahoo.co.jp>
2023-08-09 12:20:57 +0900

    プロトタイプ投稿機能

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -6,7 +6,12 @@ class PrototypesController < ApplicationController
     @prototype = Prototype.new
   end
 
+  def create
+    Prototype.create(prototype_params)
+    redirect_to '/'
+  end
+
   def prototype_params
-    params.require(:prototype).permit(:name, :image, :text).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :catch_copy, :concept).merge(user_id: current_user.id)
   end
 end
